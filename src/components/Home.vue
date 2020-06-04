25d77f454609dfac78de1879a8bac9795e14d9f6
ChengyunShi <cshi5131@usc.edu>
2020-06-03 23:53:33 -0700

    add new element

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">log out</el-button>
-    </div>
+    <el-container>
+        <el-header>Header<el-button type="info" @click="logout">log out</el-button></el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import 'element-ui/lib/theme-chalk/index.css';
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Aside, Main, Header } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Header)
 Vue.prototype.$message = Message
 
